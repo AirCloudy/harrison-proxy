d8beb8f50c91f8b9d06671e151448dae4e82fb60
hllywdnghts <harrison.obeid@icloud.com>
2019-11-13 12:04:20 -0800

    Updated proxy server with post route

--- server/index.js
+++ server/index.js
@@ -1,9 +1,6 @@
 let express = require('express');
-let request = require('request');
+let requestPromise = require('request-promise');
 let path = require('path');
-
-//server is supposed to handle a get request to it
-// when it receives a request, it sends requests to all of the services it needs to contact
 
 const PORT = 4000;
 
@@ -12,6 +9,42 @@ let app = express();
 app.use(express.json());
 
 app.use('/:songid', express.static(path.join(__dirname, '../')));
+
+app.get('/songs/:songid', (req, res) => {
+  const songId = req.params.songid;
+  const options = {
+    uri: `http://localhost:5000/${songId}`,
+    json: true
+  };
+
+  requestPromise(options)
+    .then(response => {
+      res.send(response);
+    })
+    .catch(error => {
+      res.end(error);
+    });
+});
+
+app.post('/songs', (req, res) => {
+  const options = {
+    method: 'POST',
+    body: req.body,
+    uri: `http://localhost:5000/songs`,
+    json: true
+  };
+
+  requestPromise(options)
+    .then(response => {
+      res.status(200);
+      res.end();
+    })
+    .catch(error => {
+      res.status(500);
+      res.end();
+      throw error;
+    })
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
